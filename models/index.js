66f82cd7be24de23e8fe50df8fa1c8bed0560453
MichaelFellas <MichaelAriti94@gmail.com>
2022-03-22 19:20:26 +0800

    uncommented index

--- models/index.js
+++ models/index.js
@@ -1,29 +1,25 @@
-// const { Model, DataTypes } = require('sequelize');
-// const bcrypt = require('bcrypt');
-// const sequelize = require('../config/connection');
-// const User = require('./user');
-// const Dog = require('./dog');
-// const Activity = require('./activity');
-// const Booking = require('./booking')
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+const User = require('./user');
+const Dog = require('./dog');
+const Activity = require('./activity');
+const Booking = require('./booking')
 
-// User.hasMany(Dog, {
-//   foreignKey: "user_id",
-//   onDelete: "CASCADE",
-// });
+User.hasMany(Dog, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
 
-// Dog.belongsTo(User, {
-//   foreignKey: "user_id",
-// });
+Dog.belongsTo(User, {
+  foreignKey: "user_id",
+});
 
-// Dog.hasMany(Activity, {
-//   foreignKey: "activity_id",
-//   onDelete: "CASCADE",
-// });
+//Links dog to booking via the Activity Model//
+Dog.belongsToMany(Booking, { through: Activity, foreignKey: "dog_id" });
 
-// Activity.hasMany(Dog, {
-//   foreignKey: "dog_id",
-// });
+//Links booking to dog via the Activity Model//
+Booking.belongsToMany(Dog, { through: Activity, foreignKey: "booking_id" });
 
-
-// module.exports = { User, Dog, Activity, Booking };
+module.exports = { User, Dog, Activity, Booking };
 

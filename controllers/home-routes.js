60acd9c216baf0d83bc586ff0382b71352c07e01
MichaelFellas <MichaelAriti94@gmail.com>
2022-03-22 19:49:56 +0800

    Added basic get routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -12,4 +12,61 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/home', async (req, res) => {
+  try {
+     res.render('homepage') //RENDERS HOMEPAGE WITH MAIN
+   } catch (err) {
+     console.log(err);
+     res.status(500).json(err);
+   }
+});
+
+
+ router.get('/manage', async (req, res) => {
+  try {
+     res.render('managedogs') //RENDERS MANAGEDOGS WITH MAIN
+   } catch (err) {
+     console.log(err);
+     res.status(500).json(err);
+   }
+ });
+
+
+ router.get('/book', async (req, res) => {
+  try {
+     res.render('servicebooker') //RENDERS SERVICEBOOKER WITH MAIN
+   } catch (err) {
+     console.log(err);
+     res.status(500).json(err);
+   }
+ });
+
+
+ router.get('/contact', async (req, res) => {
+  try {
+     res.render('contact') //RENDERS LANDER WITH MAIN
+   } catch (err) {
+     console.log(err);
+     res.status(500).json(err);
+   }
+ });
+
+ router.get('/calender', async (req, res) => {
+  try {
+     res.render('calender') //RENDERS LANDER WITH MAIN
+   } catch (err) {
+     console.log(err);
+     res.status(500).json(err);
+   }
+ });
+
+router.get('/addDog', async (req, res) => {
+  try {
+     res.render('addDog') //RENDERS LANDER WITH MAIN
+   } catch (err) {
+     console.log(err);
+     res.status(500).json(err);
+   }
+});
+
 module.exports = router;

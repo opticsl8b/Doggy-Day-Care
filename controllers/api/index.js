67e4e2339c4d426f891005c8231e527c2cabb357
George Cheng <sparklyspuds@gmail.com>
2022-03-26 13:04:56 +0800

    add a dog function adding bugs pending fixing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const sessionRoutes=require("./sessionRoutes")
+const sessionRoutes = require('./sessionRoutes');
+const dogRoutes = require('./dogRoutes');
 
 router.use('/users', userRoutes);
-router.use("/session",sessionRoutes)
+router.use('/session', sessionRoutes);
+router.use('/dogs', dogRoutes);
 
 module.exports = router;

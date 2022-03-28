6a6e6782422c6ca791a7ebfd2aba75506b17eaee
MichaelFellas <MichaelAriti94@gmail.com>
2022-03-28 18:49:35 +0800

    My Appts now shows dogs name

--- controllers/api/bookingRoutes.js
+++ controllers/api/bookingRoutes.js
@@ -1,43 +1,42 @@
 const router = require('express').Router();
-const { Booking, User, Dog } = require('../../models');
-const nodemailer = require("nodemailer");
+const { Booking, User, Dog, Activity } = require('../../models');
+const nodemailer = require('nodemailer');
 
 async function sendEmail(recipient, title, message) {
-    return new Promise((resolve, reject) => {
-      var transporter = nodemailer.createTransport({
-        service: "Gmail",
-        port: 587,
-        secure: false,
-        auth: {
-          user: "dogwardbounddaycare@gmail.com",
-          pass: "Doggy829@@"
-        }
-      });
-  
-      var settings = {
-        from: '"The Team" <dogwardbounddaycare@gmail.com>',
-        to: recipient,
-        subject: title, 
-        html: message
+  return new Promise((resolve, reject) => {
+    var transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      port: 587,
+      secure: false,
+      auth: {
+        user: 'dogwardbounddaycare@gmail.com',
+        pass: 'Doggy829@@',
+      },
+    });
+
+    var settings = {
+      from: '"The Team" <dogwardbounddaycare@gmail.com>',
+      to: recipient,
+      subject: title,
+      html: message,
+    };
+    transporter.sendMail(settings, function (error, response) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log(response);
+        resolve(true);
       }
-      transporter.sendMail(settings, function (error, response) {
-        if (error) {
-          console.log(error);
-          reject(error);
-        } else {
-          console.log(response);
-          resolve(true);
-        }
-      });
-    });    
+    });
+  });
 }
 
 //TODO: Add this into booking post route where response = ok
 // DONT uncomment this yet dont want to flood emails
-// var message = `  
+// var message = `
 
 //     Hi there ${user.fname} ${user.lname} Your booking for ${user.dog.name} is confirmed with the following details
-
 
 //     ${booking.name}
 //     ${booking.day }
@@ -50,25 +49,34 @@ async function sendEmail(recipient, title, message) {
 // Create new booking
 
 router.post('/', async (req, res) => {
-
-  try {  
+  try {
     const user = await User.findByPk(req.session.user_id);
     const dog = await Dog.findByPk(req.body.dog);
-    const bookingData = await Booking.create({
-    session_datetime: req.body.daysession,
-    session_name: req.body.service,
-    dog_id: req.body.dog,
-    user_id: req.session.user_id,
-  });
-   res.status(200).json(bookingData);;
+    const bookingData = await Booking.create(
+      {
+        session_datetime: req.body.daysession,
+        session_name: req.body.service,
+        // dog_id: req.body.dog,
+        user_id: req.session.user_id,
+        // dogs: [dog],
+      }
+      // {
+      //   include: Dog,
+      //   through: Activity,
+      // }
+    );
+    const activity = await Activity.create({
+      dog_id: req.body.dog,
+      booking_id: bookingData.getId(),
+    });
+
+    res.status(200).json(bookingData);
     // DONT uncomment this yet dont want to flood emails
-   //sendEmail (user.email, "Confirmation of your Doggy Daycare Appointment", message);
-   }catch(err) {
+    //sendEmail (user.email, "Confirmation of your Doggy Daycare Appointment", message);
+  } catch (err) {
     console.log(err);
     res.status(500).json(err);
   }
 });
 
 module.exports = router;
-
-

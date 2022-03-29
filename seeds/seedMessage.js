0ca9a6d61b6dcd234348af8169ff28ee84836bb1
MichaelFellas <MichaelAriti94@gmail.com>
2022-03-29 12:26:20 +0800

    Seeds fixed to not change model structure.

--- seeds/seedMessage.js
+++ seeds/seedMessage.js
@@ -1,5 +1,3 @@
-// const { TIME } = require('sequelize/types');
-// const { User } = require('../models');
 const { Message } = require('../models');
 
 const messageData = [
@@ -7,43 +5,39 @@ const messageData = [
     title: 'Booking request for Basil',
     message_content:
       'Hi, I was wondering if you have any spots available for grooming next...',
-    // created_at: sysdate(TIME),
-    // updated_at: sysdate(TIME),
+
     user_id: 1,
   },
 
-  // {
-  //   title: 'Booking cancellation',
-  //   message_content: 'Hi, I just wanted to let you know that unfortunately, I am not going to be able to...',
-  //   created_at: sysdate(TIME),
-  //   updated_at: sysdate(TIME),
-  //   user_id: User,
-  // },
+  {
+    title: 'Booking cancellation',
+    message_content:
+      'Hi, I just wanted to let you know that unfortunately, I am not going to be able to...',
 
-  // {
-  //   title: 'Checking availability for accommodation',
-  //   message_content: 'Hi, we are planning to go on a holiday to Bali ...',
-  //   created_at: sysdate(TIME),
-  //   updated_at: sysdate(TIME),
-  //   user_id: User,
-  // },
+    user_id: 2,
+  },
 
-  // {
-  //   title: 'Obedience training',
-  //   message_content:
-  //     'Hi, I just wondering when does the period for enrolment starts? ...',
-  //   created_at: sysdate(TIME),
-  //   updated_at: sysdate(TIME),
-  //   user_id: User,
-  // },
+  {
+    title: 'Checking availability for accommodation',
+    message_content: 'Hi, we are planning to go on a holiday to Bali ...',
 
-  // {
-  //   title: 'Feedback',
-  //   message_content: 'Hi, I would like to thank...',
-  //   created_at: sysdate(TIME),
-  //   updated_at: sysdate(TIME),
-  //   user_id: User,
-  // },
+    user_id: 1,
+  },
+
+  {
+    title: 'Obedience training',
+    message_content:
+      'Hi, I just wondering when does the period for enrolment starts? ...',
+
+    user_id: 2,
+  },
+
+  {
+    title: 'Feedback',
+    message_content: 'Hi, I would like to thank...',
+
+    user_id: 5,
+  },
 ];
 
 const seedMessage = () => Message.bulkCreate(messageData);
